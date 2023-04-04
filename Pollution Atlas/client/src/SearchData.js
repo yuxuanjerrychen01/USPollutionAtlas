71cdd37270b6b4ebd83f89c8bee6a82fa90fad27
yuxuanjerrychen01 <yuxuanjerrychen01@163.com>
2023-04-04 16:08:39 -0500

    client connection

--- Pollution Atlas/client/src/SearchData.js
+++ Pollution Atlas/client/src/SearchData.js
@@ -1,19 +1,42 @@
 import { useState } from "react";
+import axios from "axios";
 
 function SearchData( {onBack} ) {
     const [data, setData] = useState("");
     const [FIPSText, setFIPSText] = useState("e.g. 017019");
     const [dateText, setDateText] = useState("e.g. yyyymmdd");
     const [polluText, setPolluText] = useState("e.g. CO");
+    const [select1, setSelect1] = useState("");
 
     const handleFIPSChange = (event) => {
         setFIPSText(event.target.value);
     };
 
-    const handleFIPSSubmit = (event) => {
+    const handleFIPSSubmit = async (event) => {
         event.preventDefault();
-        // connect to sql, request for info
-        setData("heu");
+        const response = await axios.put("http://localhost:3001/basicSearch", {
+            // withCredentials: false,
+            // params:
+                
+                    "SELECT": {
+                        "`SO2 AQI`": 1,
+                        "`CO AQI`": 1
+                    },
+                    "FROM": {
+                        "SO2": 1,
+                        "CO": 1
+                    },
+                    "WHERE": {
+                        "StateName": "'Nevada'"
+                    }
+                
+        });
+        console.log(response);
+        console.log(response.data[0]);
+        const data1 = response.data[0][`CO AQI`]
+        const data2 = response.data[0][`SO2 AQI`]
+        let output = `CO AQI: ${data1}, SO2 AQI: ${data2}`;
+        setData(output);
     };
 
     const handleDateChange = (event) => {
@@ -53,6 +76,15 @@ function SearchData( {onBack} ) {
         onBack();
     };
 
+    const handleSelectChange = (event) => {
+        setSelect1(event.target.value);
+    }
+
+    const handleSelectSubmit = (event) => {
+        event.preventDefault();
+        setData(select1);
+    }
+
     return (
         <div>
             <button onClick={handleBackClick}>
@@ -60,6 +92,8 @@ function SearchData( {onBack} ) {
             </button>
             <br></br>
             <br></br>
+
+            <h2>Basic searching.</h2>
             
             <form onSubmit={handleFIPSSubmit}>
                 <label>
@@ -100,7 +134,33 @@ function SearchData( {onBack} ) {
                 </button>
                 <br></br>
             </form>
+
             <br></br>
+
+            <h2>More detailed search.</h2>
+
+            <form onSubmit={handleSelectSubmit}>
+                <label>
+                    What data do you wish to see?
+                </label>
+                <br></br>
+              
+                <select value={select1} onChange={handleSelectChange}>
+                    <option value="Locations">Locations</option>
+                    <option value="Date">Date</option>
+                    <option value="CO">CO</option>
+                    <option value="audi">Audi</option>
+                </select>
+                <br></br>
+                <input onChange={handlePolluChange} value={polluText}/>
+                <button>
+                    submit
+                </button>
+                <br></br>
+            </form>
+
+            <br></br>
+
             <h2>Results:</h2>
             {data}
 
