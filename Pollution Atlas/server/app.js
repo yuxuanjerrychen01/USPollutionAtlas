fd2519a3bf876e0eed3f480d3a98901e3d1850e8
Michael Ortiz <michaelortiz05@hotmail.com>
2023-04-07 13:24:14 -0500

    Added insert endpoint

--- Pollution Atlas/server/app.js
+++ Pollution Atlas/server/app.js
@@ -25,39 +25,24 @@ const corsOpts = {
 
 app.use(cors(corsOpts));
 // create the connection to database
-const connection =  mysql.createConnection({
-    host: '34.122.96.91',
-    user: 'root',
-    password: 'cs411truepikachu'
-   // database: 'test'
-});
-//
-connection.query(
-    "USE USPollutionAtlas1",
-    function(err, results, fields) {
-        console.log(err);
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-    }
-);
-
+// const connection =  mysql.createConnection({
+//     host: '34.122.96.91',
+//     user: 'root',
+//     password: 'cs411truepikachu'
+//    // database: 'test'
+// });
+// //
 // connection.query(
-//     "SELECT *  FROM location",
+//     "USE USPollutionAtlas1",
 //     function(err, results, fields) {
 //         console.log(err);
 //         console.log(results); // results contains rows returned by server
-//        // console.log(fields); // fields contains extra meta data about results, if available
+//         console.log(fields); // fields contains extra meta data about results, if available
 //     }
 // );
 
-
-// // Listen on Port 5000
-// app.listen(port, () => console.info(`App listening on port ${port}`))
-
 //add the router
 app.use('/', router);
-// app.use(cors)
-
 app.get('/',(req,res) => {
    // res.sendFile(path.join(__dirname+'/Frontend/index.html'));
     res.send('HELLO WORLD')
@@ -153,14 +138,14 @@ app.put('/basicSearch', (req, res) => {
  *      ...
  *  ]
  * }
- * Example Query:
+ * Example JSON:
  * {
  *     "QUERY":  [
  *         {"UPDATE": "NO2", "SET": {"COL1":"VAL1", "COL2":"VAL2"}, "WHERE": {"COL11":"VAL12", "COL22":"VAL22"}},
  *         {"UPDATE": "O3", "SET": {"COL1":"VAL1", "COL2":"VAL2"}, "WHERE": {"COL11":"VAL12", "COL22":"VAL22"}}
  *     ]
  * }
- * Returns:
+ * Runs this query:
  * BEGIN TRANSACTION;
  * UPDATE NO2
  * SET COL1 = VAL1, COL2 = VAL2
@@ -172,16 +157,16 @@ app.put('/basicSearch', (req, res) => {
  */
 app.put('/update', (req,res) => {
     const query = req.body["QUERY"];
-    let dbQuery = `BEGIN TRANSACTION;\n`
+    let dbQuery = `BEGIN TRANSACTION;\n`;
     query.forEach((q) => {
         let update = `UPDATE ${q["UPDATE"]}`;
         let set = Object.entries(q["SET"]).map(k => {return `${k[0]} = ${k[1]}`});
-        let where = Object.entries(q["WHERE"]).map(k => { return `${k[0]} = ${k[1]}`})
-        dbQuery += `${update}\nSET ${set.join(', ')}\nWHERE ${where.join(' AND ')};\n`
+        let where = Object.entries(q["WHERE"]).map(k => { return `${k[0]} = ${k[1]}`});
+        dbQuery += `${update}\nSET ${set.join(', ')}\nWHERE ${where.join(' AND ')};\n`;
     });
     dbQuery += `COMMIT;`;
     console.log(dbQuery);
-    res.send(dbQuery)
+    res.send(dbQuery);
 });
 
 /**
@@ -195,14 +180,14 @@ app.put('/update', (req,res) => {
  *      ...
  *  ]
  * }
- * Example Query:
+ * Example JSON:
  * {
  *     "QUERY":  [
  *         {"DELETE": "NO2", "WHERE": {"COL11":"VAL12", "COL22":"VAL22"}},
  *         {"UPDATE": "O3", "WHERE": {"COL11":"VAL12", "COL22":"VAL22"}}
  *     ]
  * }
- * Returns:
+ * Runs this query:
  * BEGIN TRANSACTION;
  * DELETE FROM NO2
  * WHERE COL11 = VAL12 AND COL22 = VAL22;
@@ -212,17 +197,58 @@ app.put('/update', (req,res) => {
  */
 app.put('/delete', (req, res) => {
     const query = req.body["QUERY"];
-    let dbQuery = `BEGIN TRANSACTION;\n`
+    let dbQuery = `BEGIN TRANSACTION;\n`;
     query.forEach((q) => {
         let del = `DELETE FROM ${q["DELETE"]}`;
-        let where = Object.entries(q["WHERE"]).map(k => { return `${k[0]} = ${k[1]}`})
-        dbQuery += `${del}\nWHERE ${where.join(' AND ')};\n`
+        let where = Object.entries(q["WHERE"]).map(k => { return `${k[0]} = ${k[1]}`});
+        dbQuery += `${del}\nWHERE ${where.join(' AND ')};\n`;
     });
     dbQuery += `COMMIT;`;
     console.log(dbQuery);
-    res.send(dbQuery)
+    res.send(dbQuery);
 });
-app.put('/insert', (req, res) => {
 
+/**
+ * Inserts rows into the db
+ * Expected format for JSON (All arrays for the `VALUES` key MUST be the same length within each object):
+ * {
+ *  "QUERY":
+ *  [
+ *      {"INSERT": <Table>, "VALUES" : {<ColumnName> : [value1, value2, ...], <ColumnName> : [value1, value2, ...]}},
+ *      {"INSERT": <Table>, "VALUES" : {<ColumnName> : [value1, value2, ...], <ColumnName> : [value1, value2, ...]}},
+ *      ...
+ *  ]
+ * }
+ * Example JSON:
+ * {
+ *     "QUERY":  [
+ *         {"INSERT": "NO2", "VALUES": {"a":[1,2,3], "b":[4,5,6], "c":[7,8,9]}},
+ *         {"INSERT": "O3", "VALUES": {"alpha":[1,2,3], "beta":[4,5,6], "gamma":[7,8,9]}}
+ *     ]
+ * }
+ * Runs this query:
+ * BEGIN TRANSACTION;
+ * INSERT INTO NO2
+ * (a,b,c)
+ * VALUES
+ * (1,4,7), (2,5,8), (3,6,9);
+ * INSERT INTO O3
+ * (alpha,beta,gamma)
+ * VALUES
+ * (1,4,7), (2,5,8), (3,6,9);
+ * COMMIT;
+ */
+app.put('/insert', (req, res) => {
+    const query = req.body["QUERY"];
+    let dbQuery = `BEGIN TRANSACTION;\n`;
+    query.forEach((q) => {
+        const zip = (...arr) => Array(Math.max(...arr.map(a => a.length))).fill().map((_,i) => arr.map(a => a[i]));
+        let columnNames = Object.keys(q["VALUES"]);
+        let formattedValues = zip(...Object.values(q["VALUES"])).map(e => {return `(${e.toString()})`});
+        dbQuery += `INSERT INTO ${q["INSERT"]}\n(${columnNames.toString()})\nVALUES\n${formattedValues.join(', ')};\n`;
+    });
+    dbQuery += `COMMIT;`;
+    console.log(dbQuery);
+    res.send(dbQuery);
 });
 app.listen(process.env.PORT || 3002, console.info(`App listening on port ${process.env.PORT}`));
